2ca895130296bc780905f55ac01b8da98815f4db
connorabbas <connora@jdsindustries.com>
2022-01-30 13:39:01 -0600

    dark mode

--- app/views/home.php
+++ app/views/home.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="row my-5">
         <div class="col">
-            <h1 class="mb-3">Welcome to PHP Mini Framework!</h1>
-            <h5 class="mb-4">Developed and maintained by <a href="https://github.com/connorabbas" target="_blank">Connor Abbas</a></h5>
+            <h1 class="mb-3">PHP Mini Framework</h1>
+            <h5 class="mb-5">Developed and maintained by <a href="https://github.com/connorabbas" target="_blank">Connor Abbas</a></h5>
             <p class="mb-3">Get Framework Code: <a href="https://github.com/connorabbas/php-mf" target="_blank">Here</a></p>
             <p class="mb-0">Many PHP frameworks you give you too many tools and make you comform to building out your application their way. A lot of the time you have a secific use case for your site/applicaion and fitting that vision within a predefined framework can be easier said than done. With PHP Mini Framework, the essential tools and organization you need for creating your site are provided, but the rest is customizeable and up to you!</p>
         </div>
@@ -13,7 +13,7 @@
             <ul>
                 <li>Routing for GET, POST, PATCH, PUT & DELETE HTTP requests</li>
                 <li>MVC architecture</li>
-                <li>Basic CLI actions for creating Models & Controllers</li>
+                <li>Basic CLI commands for creating Models & Controllers</li>
                 <li>Class auto loading</li>
                 <li>PDO database class</li>
                 <li>Bootstrap 5, jQuery, and React included</li>
