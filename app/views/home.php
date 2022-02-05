c16324fa65785ce4cd5ab4b9184df990e54a4056
connorabbas <connora@jdsindustries.com>
2022-02-05 09:21:34 -0600

    remove jargin

--- app/views/home.php
+++ app/views/home.php
@@ -2,9 +2,8 @@
     <div class="row my-5">
         <div class="col">
             <h1 class="mb-3">PHP Mini Framework</h1>
-            <h5 class="mb-5">Developed and maintained by <a href="https://github.com/connorabbas" target="_blank">Connor Abbas</a></h5>
+            <h5 class="mb-3">Developed and maintained by <a href="https://github.com/connorabbas" target="_blank">Connor Abbas</a></h5>
             <p class="mb-3">Get Framework Code: <a href="https://github.com/connorabbas/php-mf" target="_blank">Here</a></p>
-            <p class="mb-0">Many PHP frameworks you give you too many tools and make you comform to building out your application their way. A lot of the time you have a secific use case for your site/applicaion and fitting that vision within a predefined framework can be easier said than done. With PHP Mini Framework, the essential tools and organization you need for creating your site are provided, but the rest is customizeable and up to you!</p>
         </div>
     </div>
     <div class="row">
